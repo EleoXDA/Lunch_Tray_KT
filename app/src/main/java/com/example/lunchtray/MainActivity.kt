1d912047885787959ea5de430d9be6867f0471f0
elbay.malik <eleo.666@gmail.com>
2022-03-22 20:17:22 +0100

    Add NavController function and Cancel button function

--- app/src/main/java/com/example/lunchtray/MainActivity.kt
+++ app/src/main/java/com/example/lunchtray/MainActivity.kt
@@ -18,9 +18,11 @@ package com.example.lunchtray
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupActionBarWithNavController
 import com.example.lunchtray.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(R.layout.activity_main) {
 
     // Binding object instance corresponding to the activity_main.xml layout
     // when the view hierarchy is attached to the fragment.
@@ -33,6 +35,21 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+
         // TODO: Retrieve NavController from the NavHostFragment
+
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.navController
+
+        setupActionBarWithNavController(navController)
+    }
+
+    /**
+     *Up button function
+     */
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
+
