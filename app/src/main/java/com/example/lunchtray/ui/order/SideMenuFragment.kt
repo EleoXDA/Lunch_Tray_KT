903bedf9992e34679e577390aef4574f67ee81bc
elbay.malik <eleo.666@gmail.com>
2022-03-22 22:28:04 +0100

    Add NavController and variables

--- app/src/main/java/com/example/lunchtray/ui/order/SideMenuFragment.kt
+++ app/src/main/java/com/example/lunchtray/ui/order/SideMenuFragment.kt
@@ -21,6 +21,8 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
+import androidx.navigation.fragment.findNavController
+import com.example.lunchtray.R
 import com.example.lunchtray.databinding.FragmentSideMenuBinding
 import com.example.lunchtray.model.OrderViewModel
 
@@ -58,6 +60,8 @@ class SideMenuFragment : Fragment() {
             lifecycleOwner = viewLifecycleOwner
             viewModel = sharedViewModel
             // TODO: initialize the SideMenuFragment variables
+            sideMenuFragment = this@SideMenuFragment
+
         }
     }
 
@@ -66,6 +70,7 @@ class SideMenuFragment : Fragment() {
      */
     fun goToNextScreen() {
         // TODO: Navigate to the AccompanimentMenuFragment
+        findNavController().navigate(R.id.action_sideMenuFragment_to_accompanimentMenuFragment)
     }
 
     /**
