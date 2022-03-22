03fd893f03da2372d464ac95d5dc4baa87b21a62
elbay.malik <eleo.666@gmail.com>
2022-03-22 22:25:33 +0100

    Add NavController to revert back to Start Fragment

--- app/src/main/java/com/example/lunchtray/ui/order/CheckoutFragment.kt
+++ app/src/main/java/com/example/lunchtray/ui/order/CheckoutFragment.kt
@@ -21,6 +21,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
+import androidx.navigation.fragment.findNavController
 import com.example.lunchtray.R
 import com.example.lunchtray.databinding.FragmentCheckoutBinding
 import com.example.lunchtray.model.OrderViewModel
@@ -81,7 +82,9 @@ class CheckoutFragment : Fragment() {
         // Show snackbar to "confirm" order
         Snackbar.make(binding.root, R.string.submit_order, Snackbar.LENGTH_SHORT).show()
         // TODO: Reset order in view model
+
         // TODO: Navigate back to the [StartFragment] to start over
+        findNavController().navigate(R.id.action_checkoutFragment_to_startOrderFragment)
     }
 
     /**
